1bc0dec3c1c9b79cddc82867b76584a12cfd32d2
David ROBIN <david.a.robin@gmail.com>
2021-06-29 14:51:38 +0200

    pop nodes in a public subnet

--- lib/eks-cluster-stack.ts
+++ lib/eks-cluster-stack.ts
@@ -6,15 +6,17 @@ export class EksClusterStack extends cdk.Stack {
     super(scope, id, props);
 
     // provisiong a cluster
-const cluster = new eks.Cluster(this, 'custom-eks-cluster', {
-  version: eks.KubernetesVersion.V1_20,
-  defaultCapacity:0
-});
-cluster.addNodegroupCapacity('custom-node-group', {
-  desiredSize: 1,
-  minSize: 1,
-  instanceTypes: [ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM)],
-  remoteAccess: {sshKeyName: "eks-nodes"}
-});
+    const cluster = new eks.Cluster(this, 'custom-eks-cluster', {
+      version: eks.KubernetesVersion.V1_20,
+      defaultCapacity: 0,
+      //for testing purpose to be able to ssh directly to the node using a public IP
+      vpcSubnets: [{ subnetType: ec2.SubnetType.PUBLIC }]
+    });
+    cluster.addNodegroupCapacity('custom-node-group', {
+      desiredSize: 1,
+      minSize: 1,
+      instanceTypes: [ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM)],
+      remoteAccess: { sshKeyName: "eks-nodes" }
+    });
   }
 }
