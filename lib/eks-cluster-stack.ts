3908158d68082ac6a50495d6eb8f4485cf087e20
muldos <davidro@jfrog.com>
2022-10-07 17:08:01 +0200

    add rds instance with sg pre configured

--- lib/eks-cluster-stack.ts
+++ lib/eks-cluster-stack.ts
@@ -4,11 +4,15 @@ import { aws_eks as eks } from "aws-cdk-lib";
 import { aws_s3 as s3 } from "aws-cdk-lib";
 import { aws_iam as iam } from "aws-cdk-lib";
 import { aws_ec2 as ec2 } from "aws-cdk-lib";
+import * as rds from 'aws-cdk-lib/aws-rds';
 
 export class EksClusterStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     const prefix = this.node.tryGetContext("resourcesPrefix");
+    const dbLogin = this.node.tryGetContext("dbLogin");
+    const dbPwd = this.node.tryGetContext("dbPwd");
+
     // provisionning a cluster
     const cluster = new eks.Cluster(this, `${prefix}-cluster`, {
       version: eks.KubernetesVersion.V1_21,
@@ -16,7 +20,7 @@ export class EksClusterStack extends Stack {
     });
 
     // create a managed node group for the cluster
-    cluster.addNodegroupCapacity(`${prefix}-nodegroup`, {
+    const nodeGroup = cluster.addNodegroupCapacity(`${prefix}-nodegroup`, {
       desiredSize: 2,
       minSize: 0,
       maxSize: 3,
@@ -40,6 +44,29 @@ export class EksClusterStack extends Stack {
         "This role is used via artifactory pods service accounts to use the filestore bucket",
     });
     filestoreBucket.grantReadWrite(serviceAccRole);
+    // now let's create an RDS database in one of the private subnets of the cluster's VPC
+    //First create a security group that will allow postgresql traffic coming from eks nodes
+    // 👇 Create a SG for a web server
+    const dbSG = new ec2.SecurityGroup(this, `${prefix}-eks-db-secgrp`, {
+      vpc: cluster.vpc,
+      allowAllOutbound: true,
+      description: 'security group to allow PGSQL traffic from eks nodes',
+    });
+
+    dbSG.addIngressRule(cluster.clusterSecurityGroup,  ec2.Port.tcp(5432), 'allow postgres from cluster nodes');
+    const dbInstance = new rds.DatabaseInstance(this, `${prefix}-db`, {
+      engine: rds.DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_13_4 }),
+      // T3.medium
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MEDIUM),
+      securityGroups: [dbSG],
+      credentials: rds.Credentials.fromPassword(dbLogin, dbPwd),
+      vpc: cluster.vpc,
+      vpcSubnets: {
+        
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      }
+    });
+
     // create Cloudformation output for convenience
     new CfnOutput(this, "ArtifactoryServiceAccountRoleArn", {
       value: serviceAccRole.roleArn,
